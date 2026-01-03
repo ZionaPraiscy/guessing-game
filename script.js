cca2c5fab4ab8ba3e72b54f69a10a3caf6c20a38
ZionaPraiscy <zionapraiscy@gmail.com>
2026-01-03 22:00:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+
+let random = Math.ceil(Math.random() * 10);
+
+function checkGuess() {
+
+    let guess = parseInt(userInput.value);
+    if (guess === random) {
+        gameResult.textContent = "YOU WON!";
+        gameResult.style.backgroundColor = "green";
+    } else if (guess > random) {
+        gameResult.textContent = "TOO HIGH! TRY AGAIN.";
+        gameResult.style.backgroundColor = "#1e217c";
+    } else if (guess < random) {
+        gameResult.textContent = "TOO LOW! TRY AGAIN.";
+        gameResult.style.backgroundColor = "lightgreen";
+    } else {
+        gameResult.textContent = "Invalid Input.";
+        gameResult.style.backgroundColor = "red";
+    }
+}
